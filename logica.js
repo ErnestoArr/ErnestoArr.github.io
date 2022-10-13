4a8ef825c29cffaa0af7e78a2d3067561ab7a083
ErnestoArr <115681892+ErnestoArr@users.noreply.github.com>
2022-10-12 23:03:07 -0500

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,28 @@
+function limpiar(){
+    document.getElementById('l').reset();
+    }
+function suma(){
+    var a=parseInt(document.getElementById('n1').value);
+    var b=parseInt(document.getElementById('n2').value);
+    document.getElementById('resultado').value=a+b;
+}
+function suma(){
+    var a=parseInt(document.getElementById('n1').value);
+    var b=parseInt(document.getElementById('n2').value);
+    document.getElementById('resultado').value=a+b;
+}
+function resta(){
+    var a=parseInt(document.getElementById('n1').value);
+    var b=parseInt(document.getElementById('n2').value);
+    document.getElementById('resultado').value=a-b;
+}
+function divicion(){
+    var a=parseInt(document.getElementById('n1').value);
+    var b=parseInt(document.getElementById('n2').value);
+    document.getElementById('resultado').value=a/b;
+}
+function multiplicar(){
+    var a=parseInt(document.getElementById('n1').value);
+    var b=parseInt(document.getElementById('n2').value);
+    document.getElementById('resultado').value=a*b;
+}
